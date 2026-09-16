4a709801a8ac19938357ad879b93355efb3f5ac1
Deanna Earley <dee@earlsoft.co.uk>
2026-09-16 18:41:59 +0100

    (Slightly) nicer location display

--- extension/popup.js
+++ extension/popup.js
@@ -1,12 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const urlEl = document.getElementById('url');
-  const latitudeEl = document.getElementById('latitude');
-  const longitudeEl = document.getElementById('longitude');
+  const locationEl = document.getElementById('location');
 
   const googleLink = document.getElementById('googlelink');
 
-  function show(url) {
-    urlEl.textContent = url || 'n/a';
+  function show(message) {
+    locationEl.textContent = message || 'n/a';
   }
 
   if (typeof chrome === 'undefined' || !chrome.tabs) {
@@ -22,8 +20,10 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     const { lat, lng, zoom } = extract(tab.url);
     if (lat && lng) {
-      latitudeEl.textContent = lat || 'n/a';
-      longitudeEl.textContent = lng || 'n/a';
+      const latString = parseFloat(lat).toFixed(6);
+      const lngString = parseFloat(lng).toFixed(6);
+      const locationString = `${latString}° ${lngString}°`;
+      locationEl.textContent = locationString;
 
       googleLink.href = google_generate(lat, lng, zoom);
 
