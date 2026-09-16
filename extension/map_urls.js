779e4e93c9df3d4f94e0bf279b80a57635cf1add
Deanna Earley <dee@earlsoft.co.uk>
2026-09-16 18:58:52 +0100

    Add NLS links

--- extension/map_urls.js
+++ extension/map_urls.js
@@ -3,6 +3,8 @@ extract = function (url) {
     return google_extract(url);
   } else if (osm_match(url)) {
     return osm_extract(url);
+  } else if (nls_match(url)) {
+    return nls_extract(url);
   }
   return { lat: 0, lng: 0, zoom: 0 };
 }
@@ -26,6 +28,32 @@ google_extract = function (url) {
 
 google_generate = function (lat, lng, zoom) {
   return `https://www.google.com/maps/@${lat},${lng},${zoom}z`;
+}
+
+// NLS
+// https://maps.nls.uk/geo/explore/#zoom=18.2&lat=51.35210&lon=-0.42931&layers=269s&b=GoogleSat&o=66
+// https://maps.nls.uk/geo/explore/side-by-side/#zoom=18.2&lat=51.35176&lon=-0.42815&layers=269s&right=ESRIWorld
+
+nls_match = function (url) {
+  return url.match(/maps\.nls\.uk\/geo/);
+}
+
+nls_extract = function (url) {
+  const match = url.match(/#zoom=(-?[\d.]+)&lat=(-?[\d.]+)&lon=(-?[\d.]+)/);
+  if (match) {
+    const lat = match[2];
+    const lng = match[3];
+    const zoom = match[1];
+    return { lat, lng, zoom };
+  }
+}
+
+nls_generate_geo = function (lat, lng, zoom) {
+  return `https://maps.nls.uk/geo/explore/#zoom=${zoom}&lat=${lat}&lon=${lng}`;
+}
+
+nls_generate_sxs = function (lat, lng, zoom) {
+  return `https://maps.nls.uk/geo/explore/side-by-side/#zoom=${zoom}&lat=${lat}&lon=${lng}`;
 }
 
 // OSM
--- extension/popup.js
+++ extension/popup.js
@@ -3,6 +3,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const googleLink = document.getElementById('googlelink');
   const osmLink = document.getElementById('osmlink');
+  const nlsGeoLink = document.getElementById('nlsgeolink');
+  const nlsSxsLink = document.getElementById('nlssxslink');
 
   function show(message) {
     locationEl.textContent = message || 'n/a';
@@ -28,6 +30,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
       googleLink.href = google_generate(lat, lng, zoom);
       osmLink.href = osm_generate(lat, lng, zoom);
+      nlsGeoLink.href = nls_generate_geo(lat, lng, zoom);
+      nlsSxsLink.href = nls_generate_sxs(lat, lng, zoom);
 
     } else {
       show('Unrecognized URL');
